76c99ff69d032aae0d2462ec69743c36051378d0
Damir-Che <chechuhov.damir@mail.ru>
2020-08-22 12:18:30 +0300

    start

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_080502) do
+ActiveRecord::Schema.define(version: 2020_08_22_085121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
-    t.integer "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
