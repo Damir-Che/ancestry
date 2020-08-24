56fc24980c898a51ba84488e7baad0e9394062ce
Damir-Che <chechuhov.damir@mail.ru>
2020-08-24 13:39:27 +0300

    start4-comments

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,7 @@ class CategoriesController < ApplicationController
 
 
   def show
+    @posts = Post.where(category_id: [@category.subtree_ids]).paginate(page: params[:page], per_page: 5)
   end
 
   def new
